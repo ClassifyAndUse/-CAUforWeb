15595167c8db9023d558a56f61e8e379ab4adbae
Zhizhong Yan <A2564011261@163.com>
2024-04-06 16:17:20 +0800

    add(all): add functions

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
+<?php $username = "USER"; ?>
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=yes">
     <title>ClassifyAndUse</title>
     <link rel="stylesheet" type="text/css" href="css/userCapsule.css">
-    <link rel="stylesheet" type="text/css" href="css/indexBar.css">
+    <link rel="stylesheet" type="text/css" href="css/dockBar.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
-    <div class="indexBar"></div>
-    <div class="bar"></div>
-    <a class="username" href="user.php">Mryan2005</a>
-    <img class="gavater">
+    <div class="dockBar"></div>
+    <div class="bar">
+        <div class="gravatarBox">
+            <img class="gravatar">
+            <div class="username"><?php echo $username; ?></div>
+        </div>
+    </div>
 </body>
